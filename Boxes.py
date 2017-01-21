e2b6101c6752298844ad33bde746065ae6aa93e1
gordonseto <gordon.seto@gmail.com>
2017-01-21 14:51:01 -0800

    added displayHud()

--- Boxes.py
+++ Boxes.py
@@ -6,6 +6,8 @@ class BoxesGame():
 		pass
 
 		pygame.init()
+		pygame.font.init()
+
 		width, height = 389, 489
 
 		#initialize the screen
@@ -20,6 +22,11 @@ class BoxesGame():
 
 		self.initGraphics()
 
+		self.turn = True
+		self.me = 0
+		self.otherplayer = 0
+		self.didiwin = False
+
 	def initGraphics(self):
 		self.normallinev = pygame.image.load("normalline.png")
 		self.normallineh = pygame.transform.rotate(pygame.image.load("normalline.png"), -90)
@@ -27,6 +34,48 @@ class BoxesGame():
 		self.bar_doneh = pygame.transform.rotate(pygame.image.load("bar_done.png"), -90)
 		self.hoverlinev = pygame.image.load("hoverline.png")
 		self.hoverlineh = pygame.transform.rotate(pygame.image.load("hoverline.png"), -90)
+
+		self.separators = pygame.image.load("separators.png")
+		self.redindicator = pygame.image.load("redindicator.png")
+		self.greendindicator = pygame.image.load("greenindicator.png")
+		self.greenplayer = pygame.image.load("greenplayer.png")
+		self.blueplayer = pygame.image.load("blueplayer.png")
+		self.winningscreen = pygame.image.load("youwin.png")
+		self.gameover = pygame.image.load("gameover.png")
+		self.score_panel = pygame.image.load("score_panel.png")
+
+		#draw seperators
+		for x in range(7):
+			for y in range(7):
+				self.screen.blit(self.separators, [x*64, y*64])
+
+	def drawHUD(self):
+		#draw the background for the bottom:
+		self.screen.blit(self.score_panel, [0, 389])
+
+		#create font
+		myfont = pygame.font.SysFont(None, 32)
+
+		#create text surface
+		label = myfont.render("Your Turn:", 1, (255, 255, 255))
+
+		#draw surface
+		self.screen.blit(label, (10, 400))
+
+		self.screen.blit(self.greendindicator, (130, 395))
+
+		myfont64 = pygame.font.SysFont(None, 64)
+		myfont20 = pygame.font.SysFont(None, 20)
+
+		scoreme = myfont64.render(str(self.me), 1, (255, 255, 255))
+		scoreother = myfont64.render(str(self.otherplayer), 1, (255, 255, 255))
+		scoretextme = myfont20.render("You", 1, (255, 255, 255))
+		scoretextother = myfont20.render("Other Player", 1, (255, 255, 255))
+
+		self.screen.blit(scoretextme, (10, 425))
+		self.screen.blit(scoreme, (10, 435))
+		self.screen.blit(scoretextother, (280, 425))
+		self.screen.blit(scoreother, (340, 435))
 
 	def drawBoard(self):
 		for x in range(6):
@@ -52,6 +101,7 @@ class BoxesGame():
 
 		#draw the board
 		self.drawBoard()
+		self.drawHUD()
 
 		for event in pygame.event.get():
 			#quit if the quit button was pressed
@@ -88,7 +138,7 @@ class BoxesGame():
 		else:
 			alreadyplaced = False
 
-		if pygame.mouse.get_pressed()[0] and not alreadyplaced:
+		if pygame.mouse.get_pressed()[0] and not alreadyplaced and not isoutofbounds:
 			if is_horizontal:
 				self.boardh[ypos][xpos] = True
 			else:
